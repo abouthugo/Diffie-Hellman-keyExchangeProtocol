79af84e2e5e6a8190a4ff3371b32c2618bfaa703
Hugo Perdomo <hugo@ny-michael-w8p.stetson1865.local>
2018-11-14 10:53:59 -0500

    Updated the way g and chosenNum were generated, and readme file update

--- README.md
+++ README.md
@@ -10,8 +10,10 @@ java Program 5
 ```
 ## The User Class
 
-This class is used to generate users based on a large prime number **p **and a random number **g** member of the set {1,2,3,...,p}.
+This class is used to generate users based on a large prime number **p** and a random number **g** member of the set {2,3,...,p-1}.
+This class initializes a variable *chosenNum* which is a random integer member of the set  <img src="https://latex.codecogs.com/gif.latex?Z_p" />.
 
+Other methods are specified and you can read the source code to find out what they do. 
 ## The Program Class
 
 This class is used to wrap around the user class and grab input from the user to perform the operations necessary to implement the protocol.
--- src/Program.java
+++ src/Program.java
@@ -20,7 +20,7 @@ public class Program {
 
         int bitSize = 0; // Initialize this because the code after try/catch clause ran into some issues
 
-        /* Catch user errors!! */
+        /* Catch user input errors!! */
         try {
             bitSize = Integer.parseInt(args[0]); // Save input from cli program call
         } catch (NumberFormatException | ArrayIndexOutOfBoundsException exc){
@@ -29,9 +29,10 @@ public class Program {
         }
 
         BigInteger p = getPrime(bitSize);
-        BigInteger g= new BigInteger(bitSize, rand);
-        g = clean(g, p.add(new BigInteger("1"))); // In order to make it inclusive we need to add 1 to p
+        BigInteger g = new BigInteger(bitSize, rand);
+        g = clean(g, p); // In order to make it inclusive we need to add 1 to p
 
+        // Bob and Alice agree on a prime number p and a random g member of the set {2,3,...,p-1}
         User Alice = new User(p, g);
         User Bob = new User(p, g);
 
@@ -39,12 +40,12 @@ public class Program {
         System.out.println("g " + g);
         System.out.println("Alice selected: " + Alice.getChosenNum());
         System.out.println("Bob selected: " + Bob.getChosenNum());
-        Alice.generateMessage();
-        System.out.println("A sent to Bob: " + Alice.getMessage());
-        Bob.generateMessage();
-        System.out.println("B sent to Alice: " + Bob.getMessage());
-        System.out.println("Shared key (Bob): " + Bob.decryptKey(Alice.getMessage()));
-        System.out.println("Shared key (Alice): " + Alice.decryptKey(Bob.getMessage()));
+        Alice.generateMessage(); // Generate the message to send to Bob: g^a mod p
+        System.out.println("Alice sent to Bob: " + Alice.getMessage());
+        Bob.generateMessage(); // Generate the message to send to Alice: g^b mod p
+        System.out.println("Bob sent to Alice: " + Bob.getMessage());
+        System.out.println("Bob got from Alice key: " + Bob.decryptKey(Alice.getMessage()));
+        System.out.println("Alice got from Bob key: " + Alice.decryptKey(Bob.getMessage()));
 
     }
 
@@ -56,12 +57,12 @@ public class Program {
     }
 
     /*
-     * Returns a BigInteger that is a member of the set {1, 2, 3, ..., bound-1}
+     * Returns a BigInteger that is a member of the set {2, 3, ..., bound-1}
      */
     private static BigInteger clean(BigInteger num, BigInteger bound){
         BigInteger res = num.mod(bound);
-        if(res.intValue() == 0){
-            return new BigInteger("1");
+        if(res.intValue() <= 1){
+            return new BigInteger("2");
         }else {
             return res;
         }
